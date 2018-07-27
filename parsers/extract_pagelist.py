c9f29949899ca7adb820f6770d4c65e8ac758ea9
tunder <tunder@illinois.edu>
2018-07-27 15:59:17 -0500

    extract utility

---
+++ parsers/extract_pagelist.py
@@ -0,0 +1,35 @@
+# extract_pagelist.py
+
+# This opens a zipfile and reads all the pages;
+# it returns a list of pages, where each page is 
+# a list of lines.
+
+# To specify a zipfile, the function extract()
+# requires a "suffix": this is the part of the
+# volume id after the first period.
+
+from zipfile import ZipFile
+import os, sys
+
+def extract(suffix):
+	datadir = '/media/secure_volume/volsinuse/'
+	zippath = datadir + suffix + '.zip'
+
+	pages = []
+
+	with ZipFile(zippath) as z:
+		pageinfo = z.infolist()
+		for p in pageinfo:
+			pg = z.read(p.filename)
+			lines = pg.split('\n')
+			pages.append(lines)
+
+	return pages
+
+if __name__ == "__main__":
+	suffix = sys.argv[1]
+	pages = extract(suffix)
+	print(pages[10: 12])
+
+
+
