d3e8ff727ba886028007b1d916e513bfa2e79ed5
tedunderwood <tunder@illinois.edu>
2019-10-26 18:23:22 -0400

    fine-tuning

--- parsers/brd/brd_final_masterprocess.py
+++ parsers/brd/brd_final_masterprocess.py
@@ -2,26 +2,22 @@
 
 import os, sys, csv
 
-outfile = '/media/secure_volume/brd/output/' + sys.argv[1]
 quotefieldnames = ['bookauthor', 'booktitle', 'brdpage', 'price', 'publisher', 'publication',
                 'sentiment', 'citation', 'quote']
-
-if not os.path.isfile(outfile):
-    with open(outfile, mode = 'w', encoding = 'utf-8') as f:
-        f.write('\t'.join(quotefieldnames) + '\n')
 
 import brd_bookmaker as bookmaker
 import brd_quotationmaker as quotationmaker
 import brd_extract_pagelist as extractor
 
-vols2parse = [('39015078260935', 9)]
-
+vols2parse = [('30112013681652', 9)]
+outfile = '/media/secure_volume/brd/output/q' + vols2parse[0][0] + '.tsv'
+ 
 for vol, startpage in vols2parse:
     pagelist = extractor.extract(vol, startpage)
     books, author_errors = bookmaker.get_books(pagelist)
     quotations = quotationmaker.divide_into_quotations(books)
 
-    with open(outquotes, mode = 'w', encoding = 'utf-8') as f:
+    with open(outfile, mode = 'w', encoding = 'utf-8') as f:
         writer = csv.DictWriter(f, delimiter = '\t', fieldnames = quotefieldnames)
         writer.writeheader()
 
