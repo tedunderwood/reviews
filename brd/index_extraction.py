95fc4128256702e2794e392f934a0512cb283627
Wenyi Shang <pkuarsene@gmail.com>
2019-11-29 20:50:19 -0600

    new index extractor (1925 done)

--- brd/index_extraction.py
+++ brd/index_extraction.py
@@ -5,13 +5,13 @@ import brd_index_extract_pagelist as extractor
 np.set_printoptions(threshold=np.inf)
 
 # a list of texts that wrongly attributed to headings. Keep updating
-wrong_headings = ['Fiction-Adventure-Continual','Collections','Brgggn, A. Wind between the', 'Underngian. F. B. On a passing', 'Richardson, H: H. Fortunes of',
+wrong_headings = ['Collections','Brgggn, A. Wind between the', 'Underngian. F. B. On a passing', 'Richardson, H: H. Fortunes of',
                   'Military servitude and', 'Humphrey, Z. Home', 'Mr', 'States in war-time', 'Strange story of William',
                   'Son of', 'Island of', 'Nathan. it', 'Adventures in', 'When the king losses his',
                   'Short stories from', 'Famous detective']
 headings_with_subheading = ['European war', 'Historical novels', 'Locality, Novels of', 'Legends and folktale']
 # a list of subheadings, ususally name of places. Keep updating
-subheadings = ['Ciechoslovakian','Sicily','South sens','Syria','Viri/inia','Cambridge','Cave Cod','Florence','Illinois','InAo China','Koine','New Enylanil','Rtusia','Acadia','Poltoh','Stoediih','Metrico','New York','Borne','Vienna','Wala','West Indiei','Indo-CMna','Iowa','Lapand','London','Manitoba','Xew Enuland','New Guinea','New Jersey','Balkan states','Went Indies','Algiers','Berlin','Burma','Esthonia','Ethiopia','Hawaii','Ktissian','West Indies','Louisville','Hear East','Hew England','Salem','Singapore','Koutlt Africa','South Dakota','Gaul','Argentina','British Columbia','Brittany','Budapest','Crotia','Glasgow','Isle of Man','Hungarian','Portuguete','Servian','Nor wan','Quebec','San Francitco','Texas','Watts','Ceylon','Ituly','Uaryland','Massachusetts','Nevada','Hew Knylunil','Xortli Carolina','America','Danith','Finnish','Roumanian','Kussian','Vermont','Wett Virginia','Wiscontin','Son Francisco','Siberia','Bovth Africa','Snain','Tat mania','Tennessee','Venezuela','Mississippi','Montana','Weie Orleani','New Zealand','Norway','Porte','Rangoon','BraMil','Brooklyn','Colorado','Corsica','Dakota','Epvft','Par Bait','Finland','Java','Maine','Hussion','United State t','Hunyary','Parit','Byzantium','Mii/ille aget','Ultt0vri','Netli erlandt','Portuyal','Balkan', 'Itoumunian', 'New Mexico', 'Philippine Islands', 'Imeden', 'Syracuse', 'Algeria', 'Baltimore',
+subheadings = ['Chinese','Chechoslovakian','PoUth','West Indict','Witconsin','Wyominu','Yucatan','Neic England','Hew Guinea','Oklahoma','Ontario','Persia','Rio de Janeiro','Venl&s','Monte Carlo','Near Eatt','Uatne','Ualava','Manila','Uari/land','Mosaachutettt','Jfeoico','Uitslttippi','Greenland','Indo China','Isle of Uan','Kantas','Lapand','Long Itland','Louiiiana','Alaika','Borneo','Itrilisli Columbia','Charleston','Crotia','Esthonla','Far Edit','Oaul','Home','Scandinavia','Wett Indies','Ciechoslovakian','Sicily','South sens','Syria','Viri/inia','Cambridge','Cave Cod','Florence','Illinois','InAo China','Koine','New Enylanil','Rtusia','Acadia','Poltoh','Stoediih','Metrico','New York','Borne','Vienna','Wala','West Indiei','Indo-CMna','Iowa','Lapand','London','Manitoba','Xew Enuland','New Guinea','New Jersey','Balkan states','Went Indies','Algiers','Berlin','Burma','Esthonia','Ethiopia','Hawaii','Ktissian','West Indies','Louisville','Hear East','Hew England','Salem','Singapore','Koutlt Africa','South Dakota','Gaul','Argentina','British Columbia','Brittany','Budapest','Crotia','Glasgow','Isle of Man','Hungarian','Portuguete','Servian','Nor wan','Quebec','San Francitco','Texas','Watts','Ceylon','Ituly','Uaryland','Massachusetts','Nevada','Hew Knylunil','Xortli Carolina','America','Danith','Finnish','Roumanian','Kussian','Vermont','Wett Virginia','Wiscontin','Son Francisco','Siberia','Bovth Africa','Snain','Tat mania','Tennessee','Venezuela','Mississippi','Montana','Weie Orleani','New Zealand','Norway','Porte','Rangoon','BraMil','Brooklyn','Colorado','Corsica','Dakota','Epvft','Par Bait','Finland','Java','Maine','Hussion','United State t','Hunyary','Parit','Byzantium','Mii/ille aget','Ultt0vri','Netli erlandt','Portuyal','Balkan', 'Itoumunian', 'New Mexico', 'Philippine Islands', 'Imeden', 'Syracuse', 'Algeria', 'Baltimore',
                'BroOklyn', 'Detroit', 'Hawaiian Islands', 'Long Island', 'Heeico', 'Michigan', 'Flem\xef\xac\x82h',
                'Prussia', 'Austria', 'Balkans', 'Hungary', 'Poland', 'Asiatic Turkey', 'Austrla-Hungary', 'Brazll',
                'Richard', 'Georgla', 'Great Lakes', 'Clammer and', 'Newport', 'South Sea Islands', 'Turkey', 'Bohemia',
@@ -163,7 +163,7 @@ for year, suffix in year_suffix:
                                 if (text[k + j][:4] in ['See ', 'Sec ', 'Sac ']):
                                     continue
                             if len(text[j + k]) >= 9:  # not "xxx-Continued"
-                                if (text[j + k][-9:] in ['Continued', 'Continucd','Uontinued',]):
+                                if (text[j + k][-9:] in ['Continued', 'Continucd','Uontinued','Continual']):
                                     continue
                             if (text[k + j] in subheadings or text[
                                 k + j] in wrong_headings):  # For wrong headings, ignore them
@@ -303,7 +303,7 @@ for year, suffix in year_suffix:
                                 if (text[k + j][:4] in ['See ', 'Sec ', 'Sac ']):
                                     continue
                             if len(text[j + k]) >= 9:  # not "xxx-Continued"
-                                if (text[j + k][-9:] in ['Continued', 'Continucd','Uontinued']):
+                                if (text[j + k][-9:] in ['Continued', 'Continucd','Uontinued','Continual']):
                                     continue
                             if (text[k + j] in subheadings or text[
                                 k + j] in wrong_headings):  # For wrong headings, ignore them
@@ -337,7 +337,7 @@ for year, suffix in year_suffix:
                             k])  # print headings and all the content of each heading
                         count = -1
                         for m in list(range(len(fiction_books[k]))):
-                            if(fiction_headings[k]=='Young people' or fiction_headings[k]=='Field, Eugene'):
+                            if(fiction_headings[k]=='Young people' or fiction_headings[k]=='Field, Eugene' or fiction_headings[k]=='Zionism'):
                                 print(fiction_books[k][m])
                             if ((re.match(pattern1, fiction_books[k][m]) != None) or (re.match(pattern1_3,
                                                                                                fiction_books[k][
