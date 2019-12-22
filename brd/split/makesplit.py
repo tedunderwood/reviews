b96f073fac631d36b1b94b27e47ccb30f46d74d3
tedunderwood <tunder@illinois.edu>
2019-12-21 21:32:34 -0600

    data munging

---
+++ brd/split/makesplit.py
@@ -0,0 +1,23 @@
+import sys
+
+args = sys.argv
+
+infile = '/media/secure_volume/brd/output_index/volume' + str(args[1]) + ' ' + str(args[2]) + '.txt'
+
+outfolder = '/media/secure_volume/brd/split/'
+
+with open(infile, encoding = 'utf-8') as f:
+    lines = f.readlines()
+
+numlines = len(lines)
+
+fileindex = 0
+
+for floor in range(0, numlines, 48):
+    filename = outfolder + 's' + str(fileindex) + '.txt'
+    fileindex += 1
+
+    with open(filename, mode = 'w', encoding = 'utf-8') as f:
+        for l in lines[floor: floor + 48]:
+            outline = ' ' + l.replace('$', ' <:> ')
+            f.write(outline)
