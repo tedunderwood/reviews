5fce075c357e9f4c6992d7bf55c4db4e56381b45
Wenyi Shang <pkuarsene@gmail.com>
2020-06-28 02:56:08 -0500

    1976

--- brd/index_extraction.py
+++ brd/index_extraction.py
@@ -358,6 +358,7 @@ for year, suffix in year_suffix:
                     elif (
                             count >= 0):  # if there is already a list in the book list (which means there is already list in headings)
                         fiction_books[count].append(text[j + k])  # add texts into the booklist
+                        print(fiction_books[count])
                 length = len(fiction_headings)
 
                 fiction_authors = []  # to store the names of authors
@@ -376,7 +377,6 @@ for year, suffix in year_suffix:
                         print('<\heading "%s">' % fiction_headings[
                             k])  # print headings and all the content of each heading
                         count = -1
-                        print(fiction_books[k])
                         for m in list(range(len(fiction_books[k]))):
                             if(fiction_headings[k]=='Women' or [k]=='Young people' or fiction_headings[k]=='Youth' or fiction_headings[k]=='Zionism' or fiction_headings[k]=='Zoological gardens' or fiction_headings[k]=='Women in Industry' or fiction_headings[k]=='World war, 1939-' or fiction_headings[k]=='Weird stories' or fiction_headings[k]=='World war, 1939-1946' or fiction_headings[k]=='World war, 1939-1945' or fiction_headings[k]=='Whaling industry' or fiction_headings[k]=='Writers' or fiction_headings[k]=='Zoological specimens' or fiction_headings[k]=='Collection and preservation'):
                                 print(fiction_books[k][m])
