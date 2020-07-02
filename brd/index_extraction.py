5ca71182150a8ade961d6bee6b9c0a06e74eda25
Wenyi Shang <pkuarsene@gmail.com>
2020-07-01 20:35:09 -0500

    1982

--- brd/index_extraction.py
+++ brd/index_extraction.py
@@ -143,7 +143,7 @@ for year, suffix in year_suffix:
         text = []
         for i in range(index_startpage, index_endpage):
             for j in range(len(pagelist[i])):
-                if(volume1983==1):
+                if(volume1983==1 and i>=1620 and i<=1670):
                     print(pagelist[i][j])
                 if(volume1976_and_later==0):
                     bookcount += pagelist[i][j].count('(')  # how many books are there in the index in total
@@ -470,7 +470,7 @@ for year, suffix in year_suffix:
         print(bookcount_fiction_genre)
         print(headingcount_fiction_about)
         print(headingcount_fiction_genre)
-        print(index_startpage, index_endpage)
+        #print(index_startpage, index_endpage)
 
     if flag == 0:  # if the volume doesn't have an index, print it out and end
         print('Volume number %d: no index found in this volume' % (volume_id))
