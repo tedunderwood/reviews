005e114f084a3986837e89d6ce6c77431e06c601
tedunderwood <tunder@illinois.edu>
2022-08-06 16:47:07 -0500

    analysis

---
+++ littlemags/calculate_ratio.py
@@ -0,0 +1,63 @@
+# calculate ratio
+
+import glob
+import pandas as pd
+
+filelist = glob.glob('/media/secure_volume/clm/output/titlematch*')
+
+hits = dict()
+totals = dict()
+
+
+for afilepath in filelist:
+	alternate = afilepath.replace('titlematch', '')
+
+	allyears = pd.read_csv(alternate, sep = '\t')
+	matched = pd.read_csv(afilepath, sep = '\t')
+
+	authors = set([str(x) for x in matched.author])
+
+	for auth in authors:
+		df = matched.loc[matched.author = auth, : ]
+		pubyrs = set()
+		for years in df.yearlist:
+			combos = years.split()
+			for c in combos:
+				yr, pubyr = c.split('--')
+				yr = int(yr)
+				if yr < 80:
+					yr = yr + 1900
+				else:
+					yr = yr + 1900
+				pubyr = int(pubyr)
+				pubyrs.add(pubyr)
+				gap = yr - pubyr
+				if gap not in hits:
+					hits[gap] = 0
+				hits[gap] += 1
+
+		df = matched.loc[matched.author = auth, : ]
+		for yearstrings in df.yearlist:
+			years = [int(x) for x in yearstring.split()]
+			for yr in years:
+				if yr < 80:
+					yr = yr + 1900
+				else:
+					yr = yr + 1900
+
+				for py in pubyrs:
+					gap = yr - py
+					if gap not in totals:
+						totals[gap] = 0
+					totals[gap] += 1
+
+	print(len(hits), len(totals))
+
+
+for gap in [-5, 15]:
+	print(gap, round((hits[gap] * 100)/totals[gap]), 6)
+
+
+
+
+
